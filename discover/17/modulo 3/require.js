6e3ac35ea650fc52dab730ab6662265ab0f09501
Pedro Ricardo Oliveira Córdova <43557558+prcordova@users.noreply.github.com>
2022-05-16 23:40:59 -0300

    aula 17 - modulo 3 completo

---
+++ discover/17/modulo 3/require.js
@@ -0,0 +1,9 @@
+// modulos nativos
+// const path = require('path');
+
+// console.log(path.basename('/users/starter/docs.js'))
+
+// meus modulos
+
+const myModule = require('./exports')
+console.log(myModule)
